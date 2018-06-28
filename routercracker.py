08a26682bb65308d2ea4eac28bb74691a7d53d65
NskB3 <40596952+NskB3@users.noreply.github.com>
2018-06-28 11:18:42 +0200

    routercracker.py
    Main file

---
+++ routercracker.py
@@ -0,0 +1,22 @@
+#RouterCracker
+#Hacking routers and telnet passwords. 
+#Tool By NSK B3 aka Bence Csikos
+#######################################
+import time, os, argparse
+import fontstyles
+try:
+  import requests
+ except ImportError:
+  print "Install The requests module please."
+ def banner():
+     print("""
+     |______________|
+         |NSK B3|
+        RouterCrack
+     |--------------|
+    """) def args():
+       parser = argparse.ArgumentParser() 
+       parser.add_argument('-l', '--load', help="Load script") 
+       args = parser.parse_args() 
+banner()
+args() 
